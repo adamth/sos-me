4529368a761ce35e632d70e75383c3077f4e1ddb
Adam Thompson <Adam@Adams-MacBook-Pro.local>
2017-03-15 22:27:45 +1100

    Add authenticate middleware

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ const router        = require('express').Router();
 const {User}     = require('./../models/user');
 const _             = require('lodash');
 const {ObjectID}    = require('mongodb');
+var {authenticate}  = require('./../middleware/authenticate');
 
 // POST /users aka create a user
 router.post('/',(req,res) => {
@@ -16,6 +17,26 @@ router.post('/',(req,res) => {
         res.status(400).send(e);
     })
 
+});
+
+// POST /users/login {email, password}
+router.post('/login', (req, res) => {
+    var body = _.pick(req.body,['email', 'password']);
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth',token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+router.delete('/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.sendStatus(200);
+    },() => {
+       res.sendStatus(400); 
+    });
 });
 
 module.exports = router;
